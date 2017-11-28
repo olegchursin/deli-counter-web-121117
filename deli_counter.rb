7c2430a2a2c8d2f479b69997926c7085487f9cb1
Oleg Chursin <olegchursin@gmail.com>
2017-11-28 15:31:29 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,6 +4,6 @@ def line(katz_deli)
   if katz_deli.size == 0
     puts "The line is currently empty."
   else current_katz_deli= katz_deli.map.with_index { |person, index| "#{index+1}. #{person}"}
-    puts "The line is currently: #{current_katz_deli.join(",")}"
+    puts "The line is currently: #{current_katz_deli.join}"
   end
 end
