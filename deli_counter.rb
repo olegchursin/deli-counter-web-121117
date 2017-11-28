b63ed4bf95ce1dbf7256a2163e3a3c5cab4ed57b
Oleg Chursin <olegchursin@gmail.com>
2017-11-28 15:45:41 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -10,6 +10,6 @@ end
 
 def take_a_number(katz_deli, name)
   katz_deli << name
-  index = katz_deli.index("name")
-  puts "Welcome, #{name}. You are number #{index+1} in line."
+  i = katz_deli.index("name")
+  puts "Welcome, #{name}. You are number #{i+1} in line."
 end
