bc400f17057a53934f41d06fb7f4a2e731ad378f
Oleg Chursin <olegchursin@gmail.com>
2017-11-28 15:17:07 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,5 +3,4 @@ katz_deli = []
 def line(katz_deli)
   if katz_deli.size == 0
     puts "The line is currently empty"
-  else katz_deli.map { |i| "Hello, #{i}! Your current position in line is #{i+1}." }
 end
